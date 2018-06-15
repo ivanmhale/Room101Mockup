ae8da7a12d660d02ae7abb8c4e7b10a717eee062
Ivan <ivanmhale@gmail.com>
2018-06-14 20:44:01 -0400

    tweak

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
 const app = express();
-require('./routes')(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-const port = process.env.PORT || 5000;
+require('./routes')(app);
 
-app.listen(port);
+app.listen(process.env.PORT || 5000);
